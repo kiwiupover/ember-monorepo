17e093d025ac403c0043871bc7480f43826fad8c
Dave Laird <kiwiupover@gmail.com>
2024-09-30 07:33:13 -0700

    fixing type restoration generator

--- libraries/ui/package/.eslintrc.cjs
+++ libraries/ui/package/.eslintrc.cjs
@@ -33,12 +33,5 @@ module.exports = {
       plugins: ['ember'],
       extends: ['eslint:recommended', 'plugin:ember/recommended', 'plugin:ember/recommended-gjs'],
     },
-    {
-      files: ['tests/**/*.{js,ts,gjs,gts}'],
-      rules: {
-        // override / enable optional rules
-        'ember/no-replace-test-comments': 'error',
-      },
-    },
   ],
 };
