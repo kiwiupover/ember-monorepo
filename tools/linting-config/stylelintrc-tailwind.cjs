dd7e01416a75504938eb8a8960a57ea89c463258
Dave Laird <kiwiupover@gmail.com>
2024-10-14 21:46:41 -0700

    Adding stylelint and templatelint configuration files

---
+++ tools/linting-config/stylelintrc-tailwind.cjs
@@ -0,0 +1,10 @@
+'use strict';
+
+const { rules } = require('eslint-config-prettier');
+
+module.exports = {
+  extends: ['stylelint-config-tailwindcss', 'stylelint-prettier/recommended'],
+  rules: {
+    // Add custom rules here
+  },
+};
